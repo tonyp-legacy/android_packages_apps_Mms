0ada158ec4b9ba69a158cff2811a81d8033cf15a
tonyp <tonyptonyp@gmail.com>
2013-07-22 02:52:23 +0200

    Revert "taskon home"
    This reverts commit 7e4ae5710c0aef0103f6355242be5ee0d2c7d284.

--- src/com/android/mms/transaction/MessagingNotification.java
+++ src/com/android/mms/transaction/MessagingNotification.java
@@ -886,7 +886,6 @@ public class MessagingNotification {
             }
         }
         TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(context);
-        taskStackBuilder.setTaskOnHome(false);
 
         // If we have more than one unique thread, change the title (which would
         // normally be the contact who sent the message) to a generic one that
@@ -1292,7 +1291,6 @@ public class MessagingNotification {
         }
 
         TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(context);
-        taskStackBuilder.setTaskOnHome(false);
         if (allFailedInSameThread) {
             failedIntent = new Intent(context, ComposeMessageActivity.class);
             if (isDownload) {
